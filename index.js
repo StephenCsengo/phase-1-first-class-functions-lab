889306589ae4ca7a82d3cdfc1e4b37e323e31cc3
Stephen Csengo <stephencsengo@gmail.com>
2023-03-25 14:27:34 -0400

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier)
+{
+    const fareMultiplier = fare => fare*multiplier;
+    
+    return fareMultiplier;
+}
+
+const fareDoubler = function (fare) {
+    const multiplier = 2;
+    return createFareMultiplier(multiplier)(fare);
+}
+
+const fareTripler = function (fare) {
+    const multiplier = 3;
+    return createFareMultiplier(multiplier)(fare);
+}
+
+function selectDifferentDrivers (arrayOfDrivers, selection) {
+    return selection(arrayOfDrivers);
+}
